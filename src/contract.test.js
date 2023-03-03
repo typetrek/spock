da00f4019a361f35ad6438366337596c83075a9d
Joshua Hoeflich <joshuaharry411@icloud.com>
2023-03-03 16:39:28 -0600

    Add tests for contracts

--- src/contract.test.js
+++ src/contract.test.js
@@ -1,4 +1,11 @@
-const { isNumber, isString, isBoolean } = require("./contract");
+const {
+  isNumber,
+  isString,
+  isBoolean,
+  isSymbol,
+  makeFlatContract,
+  ContractError,
+} = require("./contract");
 
 describe("Basic predicate functions in our contract library", () => {
   test("Work with numbers", () => {
@@ -12,5 +19,50 @@ describe("Basic predicate functions in our contract library", () => {
   test("Work with booleans", () => {
     expect(isBoolean(true)).toBe(true);
     expect(isBoolean("true")).toBe(false);
+  });
+  test("Works with symbols", () => {
+    expect(isSymbol(Symbol("hello"))).toBe(true);
+    expect(isSymbol("hello")).toBe(false);
+  });
+});
+
+describe("makeFlatContract", () => {
+  test("Returns a flat contract tagged with flat", () => {
+    expect(makeFlatContract(3).type).toEqual("FLAT");
+  });
+  test("Wraps primitives correctly", () => {
+    expect(() => makeFlatContract(3).wrap(3)).not.toThrow();
+  });
+  test("Throws when it should when given a primitive", () => {
+    expect(() => makeFlatContract(3).wrap(2)).toThrow(ContractError);
+    expect(() => makeFlatContract(3).wrap("3")).toThrow(ContractError);
+  });
+  test("Wraps predicates correctly", () => {
+    expect(() =>
+      makeFlatContract((n) => {
+        if (typeof n !== "number") {
+          return false;
+        }
+        return n % 2 === 0;
+      }).wrap(2)
+    ).not.toThrow();
+  });
+  test("Throws when it should when given a predicate", () => {
+    expect(() =>
+      makeFlatContract((n) => {
+        if (typeof n !== "number") {
+          return false;
+        }
+        return n % 2 === 0;
+      }).wrap(3)
+    ).toThrow(ContractError);
+    expect(() =>
+      makeFlatContract((n) => {
+        if (typeof n !== "number") {
+          return false;
+        }
+        return n % 2 === 0;
+      }).wrap("2")
+    ).toThrow(ContractError);
   });
 });
